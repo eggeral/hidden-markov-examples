408e8836deab0e4cc9160522a3f1045319dc27f1
Alexander Egger <alexander.egger@gmail.com>
2018-05-17 16:08:22 +0200

    Use JUnit 5 as test framework

--- src/test/kotlin/egger/software/hmm/example/aa_MarkovAssumption.kt
+++ src/test/kotlin/egger/software/hmm/example/aa_MarkovAssumption.kt
@@ -3,9 +3,10 @@ package egger.software.hmm.example
 import egger.software.hmm.*
 import egger.software.hmm.state.Weather
 import egger.software.hmm.state.Weather.*
-import io.kotlintest.matchers.plusOrMinus
-import io.kotlintest.matchers.shouldBe
-import io.kotlintest.specs.BehaviorSpec
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.closeTo
+import org.junit.Test
+import org.hamcrest.CoreMatchers.`is` as isEqualTo
 
 // The following examples are described in:
 // https://www.spsc.tugraz.at/system/files/hmm.pdf
@@ -18,94 +19,117 @@ import io.kotlintest.specs.BehaviorSpec
 //           rainy | 0.1   | 0.6   | 0.2
 //           foggy | 0.2   | 0.3   | 0.6
 
-class MarkovAssumptionExample : BehaviorSpec() {
+class MarkovAssumptionExample {
 
-    init {
-        Given("the weather probabilities table") {
-            val weatherTable = stateTransitionTable<Weather, Weather> {
+    @Test
+    fun `the weather can be predicted using the Markov assumption`() {
 
-                Sunny resultsIn (Sunny withProbabilityOf 0.8)
-                Sunny resultsIn (Rainy withProbabilityOf 0.05)
-                Sunny resultsIn (Foggy withProbabilityOf 0.15)
+        // given
 
-                Rainy resultsIn (Sunny withProbabilityOf 0.1)
-                Rainy resultsIn (Rainy withProbabilityOf 0.6)
-                Rainy resultsIn (Foggy withProbabilityOf 0.2)
+        val weatherTable = stateTransitionTable<Weather, Weather> {
 
-                Foggy resultsIn (Sunny withProbabilityOf 0.2)
-                Foggy resultsIn (Rainy withProbabilityOf 0.3)
-                Foggy resultsIn (Foggy withProbabilityOf 0.5)
+            Sunny resultsIn (Sunny withProbabilityOf 0.8)
+            Sunny resultsIn (Rainy withProbabilityOf 0.05)
+            Sunny resultsIn (Foggy withProbabilityOf 0.15)
 
-            }
-            When("the current weather is sunny") {
-                Then("the probability of tomorrow it is sunny is 0.8") {
-                    weatherTable.given(Sunny) probabilityOf Sunny shouldBe 0.8.plusOrMinus(10E-9)
-                }
-                Then("the probability of tomorrow it is rainy is 0.05") {
-                    weatherTable.given(Sunny) probabilityOf Rainy shouldBe 0.05.plusOrMinus(10E-9)
-                }
-                Then("the probability of tomorrow it is sunny and the day after it is rainy is 0.04") {
-                    // P(sunny, rainy | sunny) = P(rainy | sunny, sunny) * P(sunny | sunny)
-                    // =  P(rainy | sunny) * P(sunny | sunny) // Markov assumption  -> P (qn|qn−1, qn−2, ..., q1) = P (qn|qn−1)
-                    (weatherTable.given(Sunny) probabilityOf Rainy) * (weatherTable.given(Sunny) probabilityOf Sunny) shouldBe 0.04.plusOrMinus(10E-9)
-                }
-            }
-            When("the weather yesterday was rainy and today it is foggy") {
-                Then("the probability of tomorrow it is sunny is 0.2") {
-                    // P(sunny | foggy, rainy) = P(sunny | foggy) (using the Markov assumption)
-                    // => we do not have to take into account P(foggy | rainy)
-                    weatherTable.given(Foggy) probabilityOf Sunny shouldBe 0.2.plusOrMinus(10E-09)
-                }
-            }
-            When("the weather today is foggy") {
-                Then("the probability of it is rainy two days from now is 0.34") {
+            Rainy resultsIn (Sunny withProbabilityOf 0.1)
+            Rainy resultsIn (Rainy withProbabilityOf 0.6)
+            Rainy resultsIn (Foggy withProbabilityOf 0.2)
 
-                    // Find all possible paths and sum up probabilities
-                    //
-                    // foggy -(0.2)-> sunny -(0.05)-> rainy = 0.2 * 0.05 = 0.01
-                    // foggy -(0.3)-> rainy -(0.6)-> rainy = 0.3 * 0.6 = 0.18
-                    // foggy -(0.5)-> foggy -(0.2)> rainy = 0.5 * 0.3 = 0.15
-                    // foggy --> ??? --> rainy = 0.01 + 0.18 + 0.15 = 0.34
-                    //
-                    // In math notation
-                    // P(sunny, rainy | foggy) +
-                    // P(foggy, rainy | foggy) +
-                    // P(rainy, rainy | foggy)
+            Foggy resultsIn (Sunny withProbabilityOf 0.2)
+            Foggy resultsIn (Rainy withProbabilityOf 0.3)
+            Foggy resultsIn (Foggy withProbabilityOf 0.5)
 
-                    val probabilityOfFoggySunnyRainy =
-                            (weatherTable.given(Foggy) probabilityOf Sunny) * (weatherTable.given(Sunny) probabilityOf Rainy)
-
-                    val probabilityOfFoggyRainyRainy =
-                            (weatherTable.given(Foggy) probabilityOf Rainy) * (weatherTable.given(Rainy) probabilityOf Rainy)
-
-                    val probabilityOfFoggyFoggyRainy =
-                            (weatherTable.given(Foggy) probabilityOf Foggy) * (weatherTable.given(Foggy) probabilityOf Rainy)
-
-                    probabilityOfFoggySunnyRainy shouldBe 0.01.plusOrMinus(10E-9)
-                    probabilityOfFoggyRainyRainy shouldBe 0.18.plusOrMinus(10E-9)
-                    probabilityOfFoggyFoggyRainy shouldBe 0.15.plusOrMinus(10E-9)
-
-                    probabilityOfFoggySunnyRainy + probabilityOfFoggyRainyRainy + probabilityOfFoggyFoggyRainy shouldBe 0.34.plusOrMinus(10E-9)
-
-                    // Alternative calculation using state transition table function
-                    weatherTable.sequenceLikelihood(Foggy, Sunny, Rainy) +
-                            weatherTable.sequenceLikelihood(Foggy, Rainy, Rainy) +
-                            weatherTable.sequenceLikelihood(Foggy, Foggy, Rainy) shouldBe 0.34.plusOrMinus(10E-9)
-                }
-
-                Then("calculating the likelihood of long sequences leeds to float precision problems -> use log likelihood instead") {
-
-                    // 10.000 is indistinguishable of 100.000 if using the simple likelihood function
-                    weatherTable.sequenceLikelihood(List(10000, { Sunny })) shouldBe 1.0E-323.plusOrMinus(10E-324)
-                    weatherTable.sequenceLikelihood(List(100000, { Sunny })) shouldBe 1.0E-323.plusOrMinus(10E-324)
-
-                    // using log likelihood works better
-                    weatherTable.sequenceLogLikelihood(List(10000, { Sunny })) shouldBe (-968.0).plusOrMinus(10E-1)
-                    weatherTable.sequenceLogLikelihood(List(100000, { Sunny })) shouldBe (-9689.9).plusOrMinus(10E-1)
-
-                }
-
-            }
         }
+
+        // when / then
+        assertThat(weatherTable.given(Sunny) probabilityOf Sunny, isEqualTo(0.8))
+        assertThat(weatherTable.given(Sunny) probabilityOf Rainy, isEqualTo(0.05))
+
+
+        // when / then
+        // the probability of tomorrow it is sunny and the day after it is rainy is 0.04"
+        // P(sunny, rainy | sunny) = P(rainy | sunny, sunny) * P(sunny | sunny)
+        // =  P(rainy | sunny) * P(sunny | sunny) // Markov assumption  -> P (qn|qn−1, qn−2, ..., q1) = P (qn|qn−1)
+        assertThat((weatherTable.given(Sunny) probabilityOf Rainy) * (weatherTable.given(Sunny) probabilityOf Sunny), closeTo(0.04, 1E-9))
+
+        // when
+        // the weather yesterday was rainy and today it is foggy
+
+        // then
+        // the probability of tomorrow it is sunny is 0.2
+        // P(sunny | foggy, rainy) = P(sunny | foggy) (using the Markov assumption)
+        // => we do not have to take into account P(foggy | rainy)
+        assertThat((weatherTable.given(Foggy) probabilityOf Sunny), isEqualTo(0.2))
+
+        // when
+        // the weather today is foggy
+
+        // then
+        // the probability of it is rainy two days from now is 0.34
+
+        // Find all possible paths and sum up probabilities
+        //
+        // foggy -(0.2)-> sunny -(0.05)-> rainy = 0.2 * 0.05 = 0.01
+        // foggy -(0.3)-> rainy -(0.6)-> rainy = 0.3 * 0.6 = 0.18
+        // foggy -(0.5)-> foggy -(0.2)> rainy = 0.5 * 0.3 = 0.15
+        // foggy --> ??? --> rainy = 0.01 + 0.18 + 0.15 = 0.34
+        //
+        // In math notation
+        // P(sunny, rainy | foggy) +
+        // P(foggy, rainy | foggy) +
+        // P(rainy, rainy | foggy)
+
+        val probabilityOfFoggySunnyRainy =
+                (weatherTable.given(Foggy) probabilityOf Sunny) * (weatherTable.given(Sunny) probabilityOf Rainy)
+
+        val probabilityOfFoggyRainyRainy =
+                (weatherTable.given(Foggy) probabilityOf Rainy) * (weatherTable.given(Rainy) probabilityOf Rainy)
+
+        val probabilityOfFoggyFoggyRainy =
+                (weatherTable.given(Foggy) probabilityOf Foggy) * (weatherTable.given(Foggy) probabilityOf Rainy)
+
+        assertThat(probabilityOfFoggySunnyRainy, closeTo(0.01, 1E-9))
+        assertThat(probabilityOfFoggyRainyRainy, closeTo(0.18, 1E-9))
+        assertThat(probabilityOfFoggyFoggyRainy, closeTo(0.15, 1E-9))
+
+        assertThat(probabilityOfFoggySunnyRainy + probabilityOfFoggyRainyRainy + probabilityOfFoggyFoggyRainy, closeTo(0.34, 1E-9))
+
+        // Alternative calculation using state transition table function
+        assertThat(weatherTable.sequenceLikelihood(Foggy, Sunny, Rainy) +
+                weatherTable.sequenceLikelihood(Foggy, Rainy, Rainy) +
+                weatherTable.sequenceLikelihood(Foggy, Foggy, Rainy), closeTo(0.34, 1E-9))
     }
+
+    @Test
+    fun `calculating the likelihood of long sequences leeds to float precision problems which can be solved by using the log likelihood instead`() {
+
+        // given
+        val weatherTable = stateTransitionTable<Weather, Weather> {
+
+            Sunny resultsIn (Sunny withProbabilityOf 0.8)
+            Sunny resultsIn (Rainy withProbabilityOf 0.05)
+            Sunny resultsIn (Foggy withProbabilityOf 0.15)
+
+            Rainy resultsIn (Sunny withProbabilityOf 0.1)
+            Rainy resultsIn (Rainy withProbabilityOf 0.6)
+            Rainy resultsIn (Foggy withProbabilityOf 0.2)
+
+            Foggy resultsIn (Sunny withProbabilityOf 0.2)
+            Foggy resultsIn (Rainy withProbabilityOf 0.3)
+            Foggy resultsIn (Foggy withProbabilityOf 0.5)
+
+        }
+
+        // when / then
+        // 10.000 is indistinguishable of 100.000 if using the simple likelihood function
+        assertThat(weatherTable.sequenceLikelihood(List(10000, { Sunny })), isEqualTo(1.0E-323))
+        assertThat(weatherTable.sequenceLikelihood(List(100000, { Sunny })), isEqualTo(1.0E-323))
+
+        // using log likelihood works better
+        assertThat(weatherTable.sequenceLogLikelihood(List(10000, { Sunny })), isEqualTo(-968.0032200674957))
+        assertThat(weatherTable.sequenceLogLikelihood(List(100000, { Sunny })), isEqualTo(-9689.90439078168))
+
+    }
+
 }
